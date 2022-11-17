819605f1a9845e5829f710f7c5a93c0e4e2b567b
SunWeb07 <sunnymawani786@gmail.com>
2022-11-17 23:20:34 +0530

    crafdtgp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { useState,useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const Spinner = () => (
+  <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+);
+
+const App=()=>{
+  
+  
+  const [user,setUser] = useState({});
+  
+  const [isClick, setClick] = useState(false);  
+
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  const loadUsers = async()=>{
+    const res  = await fetch("https://reqres.in/api/users?page=1")
+    const jsres = await res.json();
+    console.log(jsres)
+    setUser(jsres);
+    setClick(true)
+
+  }
+  
+  return(
+    <div className="App">
+      <h1>Api data fetch Task</h1>
+      <button onClick={loadUsers}>Get Data</button>
+<div>
+
+  <h2> Fetch data from an api in react </h2>  
+               { loading && isClick?  <Spinner/> :(
+               
+               user['data'] && user['data'].map((value) => (
+                
+                <li item key={value.id}>
+                       <img src={value.avatar} alt=""/>
+                 <h3>Name: {value.first_name + " " + value.last_name}</h3>
+                 <p>Email: {value.email}</p>
+            
+                </li> 
+                  
+            )))}
+            
+ 
+</div>
+
+
+    </div>
+  )
+}
 export default App;
